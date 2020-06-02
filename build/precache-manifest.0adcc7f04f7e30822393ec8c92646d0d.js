4b8c1778c4b8fa04ea3f88367918404e01019106
Chi Igbokwe <ch11gbokwedevelopment@gmail.com>
2020-06-02 17:52:56 -0400

    current commit

---
+++ build/precache-manifest.0adcc7f04f7e30822393ec8c92646d0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6cf20e4466db5c69d46c4f2cd1e608",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ebe461a9e2a0032fa4c",
+    "url": "/static/css/main.d7e7a755.chunk.css"
+  },
+  {
+    "revision": "02a43e2649c057b07472",
+    "url": "/static/js/2.24716f8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.24716f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ebe461a9e2a0032fa4c",
+    "url": "/static/js/main.817627df.chunk.js"
+  },
+  {
+    "revision": "ff5af55ebba59865414c",
+    "url": "/static/js/runtime-main.cd8389bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
